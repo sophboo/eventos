3a5ca827f9711807fef78e2c0f2bf103555c6522
sophia <boonobody0@gmail.com>
2026-03-11 15:27:35 -0300

    db criado

---
+++ MVC/DB/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$dsn = 'mysql: host=localhost; dbname=eventos; charset=utf8';
+$usuario = 'root';
+$senha = '';
+
+try {
+    $pdo = new PDO($dsn, $usuario, $senha);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e){
+    echo"Erro: ".$e->getMessage();
+}
+
+?>
