c35268c6b8a99cfd6b8b4d3d0a2bd4c840ba7220
sophia <boonobody0@gmail.com>
2026-03-18 07:55:12 -0300

    adicionando evento no index

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php
 
-require_once "DB/Database.php";
+require_once "DB/database.php";
 require_once "Controller/UsuarioController.php";
-require_once "Controller/ProdutoController.php";
+require_once "Controller/EventoController.php";
 require_once "Controller/PagamentoController.php";
 
 $usuarioController = new UsuarioController($pdo);
 $usuarios = $usuarioController->listar();
 
-$ProdutoController = new ProdutoController($pdo);
-$produtos = $ProdutoController->listarProduto();
+$EventoController = new EventoController($pdo);
+$Evento = $EventoController->listarEvento();
 
 $PagamentoController = new PagamentoController($pdo);
 $pagamentos = $PagamentoController->listarPagamento($pdo);
